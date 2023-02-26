69047bcd1d21b6ee005ca6939299020b82e919e5
Gregory Davis <Gregory.Davis@autozone.com>
2023-02-25 19:47:15 -0500

    feat(live chat): set up live chat on the site

--- my-nextjs-site/app/head.tsx
+++ my-nextjs-site/app/head.tsx
@@ -5,6 +5,27 @@ export default function Head() {
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <Script src="https://cdn.jsdelivr.net/npm/container-query-polyfill@1/dist/container-query-polyfill.modern.js"></Script>
+      {process.env.NODE_ENV === "development" ? null : (
+        <>
+          <Script
+            id="gtag-init"
+            dangerouslySetInnerHTML={{
+              __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'GTM-KC2Z5C7', {
+          page_path: window.location.pathname,
+        });
+        `,
+            }}
+          />
+          <Script
+            defer
+            src={`https://www.googletagmanager.com/gtag/js?id=GTM-KC2Z5C7`}
+          />
+        </>
+      )}
       <link rel="icon" href="/favicon.ico" />
     </>
   );
