0156dd716f32d2c67bde8e98c9bbab7136eb35cc
Gregory Davis <Gregory.Davis@autozone.com>
2023-03-13 16:43:32 -0400

    feat(chatGPT): set up auth to use chat feature

--- my-nextjs-site/next.config.js
+++ my-nextjs-site/next.config.js
@@ -4,6 +4,22 @@ const nextConfig = {
     appDir: true,
     typedRoutes: true,
   },
-}
+  images: {
+    remotePatterns: [
+      // github images
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/**",
+      },
+      // google images
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
